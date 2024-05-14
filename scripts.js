ec83defa192b976843c14ed3abeac627f9aed5eb
mkhizu21 <muhammad.khizar467@gmail.com>
2024-05-15 03:28:06 +0500

    New files added

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function validateSignIn() {
         alert('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
         return false;
     }
-
+    window.location.href = "Home.html";
     return true;
 }
 
@@ -24,9 +24,7 @@ function validateSignUp() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var confirmPassword = document.getElementById('confirmPassword').value;
-    var age = document.getElementById('age').value;
-    var gender = document.getElementById('gender').value;
-    var address = document.getElementById('address').value;
+    
 
     // Regular expressions for validation
     var nameRegex = /^[a-zA-Z\s]+$/;
@@ -53,53 +51,8 @@ function validateSignUp() {
         return false;
     }
 
-    if (age < 18 || age > 100) {
-        alert('Age must be between 18 and 100');
-        return false;
-    }
-
-    if (gender === '') {
-        alert('Please select a gender');
-        return false;
-    }
-
-    if (address.trim() === '') {
-        alert('Please enter your address');
-        return false;
-    }
-
     return true;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var tabs = document.querySelectorAll('nav ul li a');
-    tabs.forEach(function(tab) {
-        tab.addEventListener('click', function(e) {
-            e.preventDefault();
-            var target = this.getAttribute('href').substring(1);
-            var forms = document.querySelectorAll('.form-container');
-            forms.forEach(function(form) {
-                form.classList.remove('active');
-            });
-            document.getElementById(target).classList.add('active');
-        });
-    });
-});
 
-
-// Script for making animation
-document.addEventListener("DOMContentLoaded", function() {
-    const text = document.getElementById('animation_text'); // Get the element containing the text
-    const content = text.textContent;
-    text.textContent = '';
-    for (let i = 0; i < content.length; i++) {
-        const span = document.createElement('span');
-        if (content[i] === ' ') { // Check if the character is a space
-            span.innerHTML = '&nbsp;'; // Use non-breaking space entity
-        } else {
-            span.textContent = content[i];
-        }
-        text.appendChild(span);
-    }
-});
 
